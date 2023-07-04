1f15a8e03182334994d7485560eafa9346feb1eb
Miqdad <miqdaddeveloper@gmail.com>
2023-07-04 22:41:38 +0700

    (feat) : Add getPost Function and refactor the class to abstract class

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -1,4 +1,13 @@
-class ApiService {
+import 'package:api_call_using_retrofit/models/post_model.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
 
+part 'api_service.g.dart';
 
+@RestApi(baseUrl: 'https://jsonplaceholder.typicode.com/')
+abstract class ApiService {
+  factory ApiService(Dio dio) = _ApiService;
+
+  @GET('posts')
+  Future<List<PostModel>> getPost();
 }
